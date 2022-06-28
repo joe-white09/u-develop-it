aa60ce09bdc326e9b1219f46987f936315783e3e
joe-white09 <joe_white09@yahoo.com>
2022-06-28 13:30:29 -0500

    connected server to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,12 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'wJhOiStEePH!90',
+        database: 'election'
+    },
+    console.log('Connected to election database.')
+);
 
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use ((req, res) => {
     res.status(404).end();
